878ba2439f85c8e99f6ff5f7a6906645005eb025
Higal Ekombe <higalekombe@gmail.com>
2025-08-07 10:38:47 +0300

    Worked on the index.js, servicer-worker.js for preparing the App for PWA and TWA readiness
    Worked on the index.js, servicer-worker.js for preparing the App for PWA and TWA readiness

--- higaal/src/index.js
+++ higaal/src/index.js
@@ -10,6 +10,14 @@ root.render(
   </React.StrictMode>
 );
 
+if('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+    .then(reg => console.log("Service Worker registered:", reg))
+    .catch(err => console.error('Service Worker registration failed:', err))
+  })
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
